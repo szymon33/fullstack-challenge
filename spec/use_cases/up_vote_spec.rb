622ad914cb7be1c3226518fa55888658443dc0f6
Szymon Miedziejko <miedziejko@gmail.com>
2017-11-04 11:32:17 +0000

    Improve an obscure test case

--- spec/use_cases/up_vote_spec.rb
+++ spec/use_cases/up_vote_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe FetchComics do
 
   context 'when a vote is given' do
     before do
-      ComicVote.create(comic_id: 123)
+      ComicVote.create(comic_id: 123, votes: 100)
     end
 
     it 'increases votes amount if ComicVote exists for the comic_id' do
@@ -12,7 +12,7 @@ RSpec.describe FetchComics do
 
       comic_votes = ComicVote.find_by(comic_id: 123)
 
-      expect(comic_votes.votes).not_to be_nil
+      expect(comic_votes.votes).to eq 101
     end
   end
 end
